0b257189814ba4a1ddde74b2ed77fd4618ed1260
Rudy Pena <rudypenajr@gmail.com>
2015-09-06 11:27:37 -0500

    feat(atlassian_exam): build html/js to comp

---
+++ js/scripts.js
@@ -0,0 +1,69 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var sidebar = document.getElementsByClassName( 'sidebar' )[0];
+  var sidebarUl = sidebar.getElementsByTagName( 'ul' )[0];
+  var sidebarContent = document.getElementsByClassName( 'sidebar-content' )[0];
+  var renderMainSection = function ( currData ) {
+    var article = document.createElement( 'article' );
+    var sidebarMCTemplate = document.getElementById( 'sidebar-main-content' ).innerHTML;
+    var renderedSidebarMCTemplate = Mustache.to_html( sidebarMCTemplate, currData );
+
+    article.className = 'current-article';
+    article.innerHTML = renderedSidebarMCTemplate;
+    sidebarContent.appendChild( article );
+  };
+  var toggleSection = function ( ) {
+    console.log( this );
+    var currentArticle = document.getElementsByClassName( 'current-article' )[0];
+    var data = jsonData.items;
+
+    // remove current article because we build new one
+    currentArticle.parentNode.removeChild(currentArticle);
+
+    for ( var i = 0; i < data.length; i++ ) {
+      var currData = data[i];
+      var id = this.getAttribute( 'data-id' );
+
+      if ( currData.Id === id ) {
+        // remove other active className
+        var active = document.getElementsByClassName( 'active' )[0];
+        active.classList.remove('active');
+
+        // add active className
+        this.className = 'active';
+
+        // update main section
+        currentArticle.innerHTML = "";
+        renderMainSection( currData );
+      }
+    }
+  };
+
+  if ( jsonData ) {
+    var data = jsonData.items;
+
+    for ( var i = 0; i < data.length; i++ ) {
+      var currData = data[i];
+
+      // sidebar
+      var li = document.createElement( 'li' );
+      var sidebarTemplate = document.getElementById( 'sidebar-item' ).innerHTML;
+      var renderedSidebarTemplate = Mustache.to_html( sidebarTemplate, currData );
+
+      li.dataset.id = currData.Id;
+      li.className = "sidebar-list-items";
+      if ( i === 0 ) {
+        li.className = 'active ';
+      }
+      li.innerHTML = renderedSidebarTemplate;
+      sidebarUl.appendChild( li );
+
+      // set up event listener
+      li.addEventListener( 'click', toggleSection );
+
+      // content
+      if ( i === 0 ) {
+        renderMainSection( currData );
+      }
+    }
+  }
+});
